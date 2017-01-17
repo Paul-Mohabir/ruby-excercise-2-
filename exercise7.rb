097950b9109bd41be2223d1ac965c1778e5e72c7
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-01-17 18:52:47 -0500

    complete

--- exercise7.rb
+++ exercise7.rb
@@ -1,12 +1,16 @@
+#1
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+#2
 students.each do |group, size|
   puts "#{group}: #{size} students"
 end
 
+#3
 students[:cohort4] = "43"
 
 puts "A new group has joined"
@@ -14,4 +18,15 @@ students.each do |group, size|
   puts "#{group}: #{size} students"
 end
 
-students.keys
+#4
+puts "Cohorts attending"
+puts students.keys
+
+#5
+students.each do |group, size|
+  students[group] = size * 1.05
+end
+puts students
+
+students.delete(:cohort2)
+puts students
